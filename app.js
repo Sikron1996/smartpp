99057c861f21d4b2cdf844c594d244d6daa74483
Sikron1996 <vadimmonarh@gmail.com>
2026-06-10 01:01:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { ethers } from "https://esm.sh/ethers@6.13.4";
 import EthereumProvider from "https://esm.sh/@walletconnect/ethereum-provider@2.17.2";
-const CONTRACT_ADDRESS="PASTE_CONTRACT_ADDRESS_HERE";
+const CONTRACT_ADDRESS="0x6900e7AC03Bb9dC998970917002Fe19162ABE5B5";
 const PROJECT_ID="fe55ea601c3e7e0925c0b33723d6b158";
 const READ_RPC="https://ethereum.publicnode.com";
 const PRICE_ETH="0.0001";
